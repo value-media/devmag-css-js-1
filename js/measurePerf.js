7759d487c86fcd88b3af130ec55bceac0a5872d8
VM TD <t.dominiak@valuemedia.pl>
2023-11-22 21:50:46 +0100

    measurePerformance - poprawka formatu liczb

--- js/measurePerf.js
+++ js/measurePerf.js
@@ -4,6 +4,8 @@ export const measurePerformance = function (func, arg, label = '') {
     const result = func(arg)
     const end = performance.now();
     result && console.log(`Result: ${result}, ${label}`);
-    console.log(`Time taken for ${func.name}: ${(end-start)} miliseconds.`);
+    console.log(`Time taken for ${func.name}: ${numFormat.format(end-start)} miliseconds.`);
     console.log('');
 }
+
+const numFormat = new Intl.NumberFormat("pl-PL", { minimumFractionDigits: 3 })
