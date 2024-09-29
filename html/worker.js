6c6a34ec95923412580fae0baca6f7d190e63eef
VM TD <t.dominiak@valuemedia.pl>
2024-09-29 22:36:38 +0200

    web-socket client

--- html/worker.js
+++ html/worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('message', function (event) {
-    console.log('message received', new Date(), event);
+    console.log('Worker: message received', new Date(), event);
     const { data } = event;
     if (!data) return;
     self.postMessage({ result: "computedResult" });
