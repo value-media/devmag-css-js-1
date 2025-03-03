388caa23466e178c47c80639a99ed77494d891b9
VM TD <t.dominiak@valuemedia.pl>
2025-03-03 21:51:18 +0100

    transducer

--- js/generators.js
+++ js/generators.js
@@ -23,3 +23,26 @@ function printText(text) {
 }
 
 printText('Hello world');
+
+const arr = new Array(100).fill(0).map((_, i) => i+1);
+
+function* paginateItems(items, pageSize = 10) {
+    for(let i = 0; i < items.length; i += pageSize) 
+        yield items.slice(i, i + pageSize);
+}
+const items = paginateItems(arr);
+// console.log(items.next().value);
+// console.log(items.next().value);
+
+const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x);
+const map = fn => (accumulator, element) => accumulator.concat(fn(element));
+const filter = fn => (accumulator, element) => fn(element) ? accumulator.concat(element) : accumulator;
+
+const xform = compose(
+    map(x => x * 2),
+    filter(x => x > 50),
+    take(5)
+);
+
+const result = transduce(xform, pushing, [], arr);
+console.log('result', result)
